085482a62fcce8bc75158a38ac710da9747822a3
werterpires <werterpires23@hotmail.com>
2024-01-07 10:49:10 -0300

    add term types repository

---
+++ src/params/terms-types/terms-types.repository.ts
@@ -0,0 +1,113 @@
+import { Injectable, NotFoundException } from '@nestjs/common'
+import { InjectKnex, Knex } from 'nestjs-knex'
+import { ErrorsService } from 'src/shared/utils/errors.service'
+import { ICreateTermType, ITermType, IUpdateTermType } from './types'
+
+@Injectable()
+export class termsTypesRepository {
+  constructor(
+    @InjectKnex() private readonly knex: Knex,
+    private errorsService: ErrorsService
+  ) {}
+
+  async createTermType(
+    createTermTypeData: ICreateTermType
+  ): Promise<ITermType> {
+    try {
+      const { termTypeName, termTypeDescription } = createTermTypeData
+
+      const [termTypeId] = await this.knex('terms_types').insert({
+        term_type_name: termTypeName,
+        term_type_description: termTypeDescription
+      })
+
+      const termType = await this.findTermTypeById(termTypeId)
+
+      return termType
+    } catch (error) {
+      throw this.errorsService.handleErrors(
+        error,
+        '#Não foi possível o tipo de erro',
+        'repository/createTermType'
+      )
+    }
+  }
+
+  async findAllTermsTypes() {
+    try {
+      const termTypeConsult = await this.knex('terms_types').select('*')
+
+      return termTypeConsult
+    } catch (error) {
+      throw this.errorsService.handleErrors(
+        error,
+        '#Não foi possível buscar todos os tipos de termo.',
+        'repository/findAllTermsTypes'
+      )
+    }
+  }
+
+  async findTermTypeById(id: number): Promise<ITermType> {
+    try {
+      const termTypeConsult = await this.knex('terms_types')
+        .first('*')
+        .where('term_type_id', id)
+
+      if (!termTypeConsult) {
+        throw new NotFoundException('#Tipo de termo não encontrada.')
+      }
+
+      return termTypeConsult
+    } catch (error) {
+      throw this.errorsService.handleErrors(
+        error,
+        '#Não foi possível buscar o tipo de termo',
+        'repository/findTermTypeById'
+      )
+    }
+  }
+
+  async updateTermTypeById(
+    updateTermTypeData: IUpdateTermType
+  ): Promise<ITermType> {
+    try {
+      const { termTypeId, termTypeDescription, termTypeName } =
+        updateTermTypeData
+      const editsQuantity = await this.knex('terms_types')
+        .update({
+          term_type_name: termTypeName,
+          term_type_description: termTypeDescription
+        })
+        .where('term_type_id', termTypeId)
+      if (editsQuantity === 0) {
+        throw new NotFoundException('#Tipo de termo não encontrado.')
+      }
+      const termType = await this.findTermTypeById(termTypeId)
+      return termType
+    } catch (error) {
+      throw this.errorsService.handleErrors(
+        error,
+        '#Não foi possível atualizar o tipo de termo',
+        'repository/updateTermTypeById'
+      )
+    }
+  }
+
+  async deleteTermTypeById(id: number): Promise<void> {
+    try {
+      const deletesQuantity = await this.knex('terms_types')
+        .delete()
+        .where('term_type_id', id)
+
+      if (deletesQuantity === 0) {
+        throw new NotFoundException('#Tipo de termo não encontrado.')
+      }
+    } catch (error) {
+      throw this.errorsService.handleErrors(
+        error,
+        '#Não foi possível deletar o tipo de termo',
+        'repository/deleteTermType'
+      )
+    }
+  }
+}
