d720f3273dd7ac0e5900d520c7a7fe8d3a806627
werterpires <werterpires23@hotmail.com>
2024-01-07 19:10:27 -0300

    add Apitag to Users controllers

--- src/basics/users/users.controller.ts
+++ src/basics/users/users.controller.ts
@@ -10,7 +10,8 @@ import {
 import { UsersService } from './users.service'
 import { CreateUserDto } from './dto/create-user.dto'
 import { IsPublic } from 'src/shared/auth/decorators/is-public.decorator'
-
+import { ApiTags } from '@nestjs/swagger'
+@ApiTags('Users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
