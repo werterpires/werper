2f9c2c677fd13383f76d692cf7ee2a8df8b13db9
werterpires <werterpires23@hotmail.com>
2024-01-07 18:57:57 -0300

    change table people

--- migrations/20240107100022_people.ts
+++ migrations/20240107100022_people.ts
@@ -2,18 +2,33 @@ import type { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.alterTable('people', (table) => {
-    table.integer('company_id').unsigned().notNullable()
+    table.integer('subscription_id').unsigned()
     table
-      .foreign('company_id')
-      .references('companies.company_id')
+      .foreign('subscription_id')
+      .references('subscriptions.subscription_id')
       .onDelete('RESTRICT')
       .onUpdate('CASCADE')
+    table.unique(['cpf', 'subscription_id'], { indexName: 'unique_cpf' })
+    table.unique(['cnpj', 'subscription_id'], { indexName: 'unique_cpf' })
+    table.unique(['email', 'subscription_id'], { indexName: 'unique_cpf' })
   })
 }
 
 export async function down(knex: Knex): Promise<void> {
   return knex.schema.alterTable('people', (table) => {
-    table.dropForeign('company_id')
-    table.dropColumn('company_id')
+    table.dropForeign('subscription_id')
+    table.dropUnique(
+      ['cpf', 'subscription_id'],
+      'unique_nota_fiscal_fornecedor'
+    )
+    table.dropUnique(
+      ['cnpj', 'subscription_id'],
+      'unique_nota_fiscal_fornecedor'
+    )
+    table.dropUnique(
+      ['email', 'subscription_id'],
+      'unique_nota_fiscal_fornecedor'
+    )
+    table.dropColumn('subscription_id')
   })
 }
