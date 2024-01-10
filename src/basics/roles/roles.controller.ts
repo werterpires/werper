0db1872bbd198ec02a0b622362135d2cf4a1fcde
werterpires <werterpires23@hotmail.com>
2024-01-10 13:09:01 -0300

    add roles controllers

--- src/basics/roles/roles.controller.ts
+++ src/basics/roles/roles.controller.ts
@@ -1,34 +1,49 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { RolesService } from './roles.service';
-import { CreateRoleDto } from './dto/create-role.dto';
-import { UpdateRoleDto } from './dto/update-role.dto';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete
+} from '@nestjs/common'
+import { RolesService } from './roles.service'
+import { CreateRoleDto } from './dto/create-role.dto'
+import { UpdateRoleDto } from './dto/update-role.dto'
+import { ErrorsService } from 'src/shared/utils/errors.service'
 
 @Controller('roles')
 export class RolesController {
-  constructor(private readonly rolesService: RolesService) {}
-
-  @Post()
-  create(@Body() createRoleDto: CreateRoleDto) {
-    return this.rolesService.create(createRoleDto);
-  }
+  constructor(
+    private readonly rolesService: RolesService,
+    private readonly errosService: ErrorsService
+  ) {}
 
   @Get()
-  findAll() {
-    return this.rolesService.findAll();
+  findAllRoles() {
+    try {
+      const roles = this.rolesService.findAllRoles()
+      return roles
+    } catch (error) {
+      throw this.errosService.handleErrors(
+        error,
+        '#Não foi possível buscar todas os Papeis',
+        'controller/findAllRoles'
+      )
+    }
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.rolesService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateRoleDto: UpdateRoleDto) {
-    return this.rolesService.update(+id, updateRoleDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.rolesService.remove(+id);
+  findRoleById(@Param('id') id: string) {
+    try {
+      const role = this.rolesService.findRoleById(+id)
+      return role
+    } catch (error) {
+      throw this.errosService.handleErrors(
+        error,
+        '#Não foi possível encontrar o Papel',
+        'controller/findRoleById'
+      )
+    }
   }
 }
--- src/basics/roles/specs/roles.controller.spec.ts
+++ src/basics/roles/specs/roles.controller.spec.ts
@@ -1,20 +1,109 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { RolesController } from '../roles.controller'
 import { RolesService } from '../roles.service'
+import * as fillers from './fillers'
+import { ErrorsService } from 'src/shared/utils/errors.service'
+import { InternalServerErrorException, NotFoundException } from '@nestjs/common'
 
 describe('RolesController', () => {
-  let controller: RolesController
+  let rolesController: RolesController
+  let mockRolesService: RolesService
+  let mockErrorsService: ErrorsService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [RolesController],
-      providers: [RolesService]
+      providers: [
+        {
+          provide: RolesService,
+          useValue: {
+            findAllRoles: jest.fn().mockReturnValue(fillers.rolesArray),
+            findRoleById: jest.fn().mockReturnValue(fillers.role1)
+          }
+        },
+        {
+          provide: ErrorsService,
+          useValue: {
+            handleErrors: jest
+              .fn()
+              .mockReturnValue(new InternalServerErrorException())
+          }
+        }
+      ]
     }).compile()
 
-    controller = module.get<RolesController>(RolesController)
+    rolesController = module.get<RolesController>(RolesController)
+    mockRolesService = module.get<RolesService>(RolesService)
+    mockErrorsService = module.get<ErrorsService>(ErrorsService)
   })
 
   it('should be defined', () => {
-    expect(controller).toBeDefined()
+    expect(rolesController).toBeDefined()
+  })
+
+  describe('findAllRoles', () => {
+    it('should be defined', () => {
+      expect(rolesController.findAllRoles).toBeDefined()
+    })
+
+    it('should return an array of roles', async () => {
+      //Act
+      const roles = await rolesController.findAllRoles()
+
+      //Assert
+      expect(roles).toEqual(fillers.rolesArray)
+      expect(mockRolesService.findAllRoles).toHaveBeenCalledTimes(1)
+    })
+
+    it('should throw a internal server error', async () => {
+      //Arrange
+      jest
+        .spyOn(mockErrorsService, 'handleErrors')
+        .mockReturnValue(
+          new InternalServerErrorException('erro estourado no error')
+        )
+      jest
+        .spyOn(mockRolesService, 'findAllRoles')
+        .mockRejectedValueOnce(
+          new InternalServerErrorException('erro extourado no service')
+        )
+      //Act
+      //Assert
+      expect(() => rolesController.findAllRoles()).rejects.toThrow(
+        InternalServerErrorException
+      )
+    })
+  })
+
+  describe('findRoleById', () => {
+    it('should be defined', () => {
+      expect(rolesController.findRoleById).toBeDefined()
+    })
+
+    it('should return a role', async () => {
+      //Act
+      const role = await rolesController.findRoleById('1')
+
+      //Assert
+      expect(role).toEqual(fillers.role1)
+      expect(mockRolesService.findRoleById).toHaveBeenCalledTimes(1)
+    })
+
+    it('should throw a internal server error', async () => {
+      //Arrange
+      jest
+        .spyOn(mockErrorsService, 'handleErrors')
+        .mockReturnValue(new NotFoundException('erro estourado no error'))
+      jest
+        .spyOn(mockRolesService, 'findRoleById')
+        .mockRejectedValueOnce(
+          new NotFoundException('erro extourado no service')
+        )
+      //Act
+      //Assert
+      expect(() => rolesController.findRoleById('1')).rejects.toThrow(
+        NotFoundException
+      )
+    })
   })
 })
