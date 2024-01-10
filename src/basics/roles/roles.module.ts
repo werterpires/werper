d19446ca44ea01c3ba35ce23eeb2a227efb14aa7
werterpires <werterpires23@hotmail.com>
2024-01-10 13:27:35 -0300

    change order of imports

--- src/basics/roles/roles.module.ts
+++ src/basics/roles/roles.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
-import { RolesService } from './roles.service';
-import { RolesController } from './roles.controller';
+import { Module } from '@nestjs/common'
+import { RolesService } from './roles.service'
+import { RolesController } from './roles.controller'
+import { RolesRepository } from './roles.repository'
+import { RolesUtils } from './roles.utils'
+import { UtilsModule } from 'src/shared/utils/utils.module'
 
+const services = [RolesService, RolesRepository, RolesUtils]
 @Module({
+  imports: [UtilsModule],
   controllers: [RolesController],
-  providers: [RolesService],
+  providers: services
 })
 export class RolesModule {}
